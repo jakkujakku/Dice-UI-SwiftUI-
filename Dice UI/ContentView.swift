5b5f981faa8ba4f9f7d58942dedca1ed15750374
jakkujakku <jakkujakku98@gmail.com>
2022-12-01 11:09:22 +0900

    Complete DiceUI

---
+++ Dice UI/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Dice UI
+//
+//  Created by (^ㅗ^)7 iMac on 2022/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                
+            VStack {
+                Spacer()
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size:  50))
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                        .padding(.horizontal)
+                }
+                .background(Color.white)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct DiceView: View {
+    
+    let n : Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
---
+++ Dice UI/Dice_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_UIApp.swift
+//  Dice UI
+//
+//  Created by (^ㅗ^)7 iMac on 2022/12/01.
+//
+
+import SwiftUI
+
+@main
+struct Dice_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
